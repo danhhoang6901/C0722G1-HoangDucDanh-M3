96f6621bc1b8fa8b793cd73968259b76e8cae0ec
danhhoang6901 <danhhoang0196@gmail.com>
2022-10-14 13:49:59 +0700

    nộp bt ngày 14/10/2022

--- sql/student_management.sql
+++ sql/student_management.sql
@@ -9,18 +9,17 @@ id int primary key auto_increment,
 -- tạo bảng teacher
 create table teacher(
 id int primary key auto_increment,
-name varchar(45),
+`name` varchar(45),
 age int,
 country varchar(45));
 -- thêm mới dữ liệu 
-insert into class (name)value('danh');
-insert into class (name)value('vinh');
-insert into teacher (name, age, country) value('quang',25,'đà nẵng');
+insert into class (`name`)value('danh'),('vinh');
+insert into teacher (`name`, age, country) value('quang',25,'đà nẵng');
 -- hiển thị dữ liệu 
 select id from class;
 select * from class;
 -- chỉnh sửa 
-update class set name = 'linh' where id = 2;
+update class set `name` = 'linh' where id = 2;
 -- xóa dữ liệu
 delete from class where id = 1;
 -- xóa bảng: drop table class;
